eba02b5fd28f66db7792d0fb9cc6c8e0fe711198
jlearns9 <142127358+jlearns9@users.noreply.github.com>
2023-08-29 00:38:49 -0500

    Final Draft

--- index.js
+++ index.js
@@ -1,52 +1,91 @@
-document.addEventListener('DOMContentLoaded', (event) => {
+let scoreHomeEl = document.getElementById("score-home-el")
+let scoreGuestEl = document.getElementById("score-guest-el")
+let foulHomeEl = document.getElementById("foul-home-el")
+let foulGuestEl = document.getElementById("foul-guest-el")
+let periodEl = document.getElementById("period-el")
 
-    let homeEl = document.getElementById("home-el");
-    let homeScore = 0;
+let scoreHome = 0
+let scoreGuest = 0
+let foulHome = 0
+let foulGuest = 0
+let period = 1
 
-    let guestEl = document.getElementById("guest-el");
-    let guestScore = 0;
-
-    function incrementScore(target, value) {
-        if (target === 'home') {
-            homeScore += value;
-            homeEl.innerText = homeScore;
-        } else if (target === 'guest') {
-            guestScore += value;
-            guestEl.innerText = guestScore;
-        }
+function updateScores() {
+    if (scoreHome > scoreGuest) {
+        scoreHomeEl.classList.add("higher-score");
+        scoreGuestEl.classList.remove("higher-score");
+    } else if (scoreGuest > scoreHome) {
+        scoreGuestEl.classList.add("higher-score");
+        scoreHomeEl.classList.remove("higher-score");
+    } else {
+        scoreHomeEl.classList.remove("higher-score");
+        scoreGuestEl.classList.remove("higher-score");
     }
+}
+// HOME FUNCTIONS //
+function homeincrement1() {
+    scoreHome += 1
+    scoreHomeEl.innerText = scoreHome
+    updateScores()
+}
 
-    function reset() {
-        homeScore = 0;
-        guestScore = 0;
-        homeEl.innerText = homeScore;
-        guestEl.innerText = guestScore;
+function homeincrement2() {
+    scoreHome += 2
+    scoreHomeEl.innerText = scoreHome
+    updateScores()
+}
+
+function homeincrement3() {
+    scoreHome += 3
+    scoreHomeEl.innerText = scoreHome
+    updateScores()
+}
+
+function homefoulincrement() {
+    foulHome += 1
+    foulHomeEl.innerText = foulHome
+}
+// GUEST FUNCTIONS //
+function guestincrement1() {
+    scoreGuest += 1
+    scoreGuestEl.innerText = scoreGuest
+    updateScores();
+}
+
+function guestincrement2() {
+    scoreGuest += 2
+    scoreGuestEl.innerText = scoreGuest
+    updateScores()
+}
+
+function guestincrement3() {
+    scoreGuest += 3
+    scoreGuestEl.innerText = scoreGuest
+    updateScores()
+}
+
+function guestfoulincrement() {
+    foulGuest += 1
+    foulGuestEl.innerText = foulGuest
+}
+// OTHER //
+function periodincrement() {
+    period += 1;
+    if (period >4) {
+        period = 1;
     }
+    periodEl.textContent = period;
+}
 
-    window.homeincrement1 = function() {
-        incrementScore('home', 1);
-    }
-
-    window.homeincrement2 = function() {
-        incrementScore('home', 2);
-    }
-
-    window.homeincrement3 = function() {
-        incrementScore('home', 3);
-    }
-
-    window.guestincrement1 = function() {
-        incrementScore('guest', 1);
-    }
-
-    window.guestincrement2 = function() {
-        incrementScore('guest', 2);
-    }
-
-    window.guestincrement3 = function() {
-        incrementScore('guest', 3);
-    }
-
-    window.reset = reset;
-
-});
+function reset() {
+    scoreHome = 0
+    scoreGuest = 0
+    foulHome = 0
+    foulGuest = 0
+    period = 1
+    scoreHomeEl.innerText = scoreHome
+    scoreGuestEl.innerText = scoreGuest
+    foulHomeEl.innerText = foulHome
+    foulGuestEl.innerText = foulGuest
+    periodEl.innerText = period
+}
